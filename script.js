dc90b938fcf2f84fb17f2b762fe749abc2177c07
Atharva Pandey <finitecode.contact@gmail.com>
2023-05-13 14:11:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ showMoreButton.addEventListener('click', () => {
   const parent = document.getElementById('additional-cards-container');
   parent.classList.toggle('hide');
 })
+
+// Select the "Show More" button
+const showMoreButton = document.querySelector('.show-more-button');
+
+// Select the last three cards
+const lastThreeCards = document.querySelectorAll('.card:nth-child(n+4)');
+
+// Hide the last three cards initially
+lastThreeCards.forEach(card => {
+  card.classList.add('hide');
+});
+
+// Add an event listener to the "Show More" button
+showMoreButton.addEventListener('click', () => {
+  // Toggle the visibility of the last three cards
+  lastThreeCards.forEach(card => {
+    card.classList.toggle('hide');
+  });
+  
+  // Change the text of the "Show More" button to "Show All" if all the cards are visible
+  const hiddenCards = document.querySelectorAll('.card.hide');
+  if (hiddenCards.length === 0) {
+    showMoreButton.textContent = 'Show All';
+  }
+});
