a35dd9fabbfe986343889d84afc6e8022da94468
Atharva Pandey <finitecode.contact@gmail.com>
2023-05-13 13:35:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<!-- JavaScript code to toggle sidebar on menu button click -->
+<script>
+    document.getElementById('menu-toggle').addEventListener('click', function () {
+        document.getElementById('mobile-menu').classList.toggle('hidden');
+    });
+</script>
