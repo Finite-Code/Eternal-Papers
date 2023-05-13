6c174c961475ab6905cb390d690a17ad52376095
Atharva Pandey <finitecode.contact@gmail.com>
2023-05-13 14:17:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-<!-- JavaScript code to toggle sidebar on menu button click -->
-document.getElementById('menu-toggle').addEventListener('click', function () {
-  document.getElementById('mobile-menu').classList.toggle('hidden');
-});
-
-const showMoreButton = document.getElementById('show-more-button');
-const additionalCards = document.querySelectorAll('.additional-card');
-
-showMoreButton.addEventListener('click', () => {
-  const parent = document.getElementById('additional-cards-container');
-  parent.classList.toggle('hide');
-})
-
-// Select the "Show More" button
-const showMoreButton = document.querySelector('.show-more-button');
-
-// Select the last three cards
-const lastThreeCards = document.querySelectorAll('.card:nth-child(n+4)');
-
-// Hide the last three cards initially
-lastThreeCards.forEach(card => {
-  card.classList.add('hide');
-});
-
-// Add an event listener to the "Show More" button
-showMoreButton.addEventListener('click', () => {
-  // Toggle the visibility of the last three cards
-  lastThreeCards.forEach(card => {
-    card.classList.toggle('hide');
+document.addEventListener('DOMContentLoaded', function() {
+  // JavaScript code to toggle sidebar on menu button click
+  document.getElementById('menu-toggle').addEventListener('click', function () {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
   });
-  
-  // Change the text of the "Show More" button to "Show All" if all the cards are visible
-  const hiddenCards = document.querySelectorAll('.card.hide');
-  if (hiddenCards.length === 0) {
-    showMoreButton.textContent = 'Show All';
-  }
+
+  const showMoreButton = document.getElementById('show-more-button');
+  const additionalCards = document.querySelectorAll('.additional-card');
+
+  showMoreButton.addEventListener('click', () => {
+    const parent = document.getElementById('additional-cards-container');
+    parent.classList.toggle('hide');
+  })
+
+  // Select the "Show More" button
+  const showMoreButton = document.querySelector('.show-more-button');
+
+  // Select the last three cards
+  const lastThreeCards = document.querySelectorAll('.card:nth-child(n+4)');
+
+  // Hide the last three cards initially
+  lastThreeCards.forEach(card => {
+    card.classList.add('hide');
+  });
+
+  // Add an event listener to the "Show More" button
+  showMoreButton.addEventListener('click', () => {
+    // Toggle the visibility of the last three cards
+    lastThreeCards.forEach(card => {
+      card.classList.toggle('hide');
+    });
+
+    // Change the text of the "Show More" button to "Show All" if all the cards are visible
+    const hiddenCards = document.querySelectorAll('.card.hide');
+    if (hiddenCards.length === 0) {
+      showMoreButton.textContent = 'Show All';
+    }
+  });
 });
