c9cce71ecdba8423414d030335d9916b15c96bf6
Atharva Pandey <finitecode.contact@gmail.com>
2023-05-13 13:47:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 <!-- JavaScript code to toggle sidebar on menu button click -->
-<script>
-    document.getElementById('menu-toggle').addEventListener('click', function () {
-        document.getElementById('mobile-menu').classList.toggle('hidden');
-    });
-</script>
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  document.getElementById('mobile-menu').classList.toggle('hidden');
+});
+
+const sidebarToggleBtn = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+sidebarToggleBtn.addEventListener('click', function() {
+  console.log('Sidebar button clicked');
+  sidebar.classList.toggle('show-sidebar');
+});
