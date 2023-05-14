fcd58134d9b340c0d615478227c061d0c3c8e1c9
Atharva Pandey <finitecode.contact@gmail.com>
2023-05-14 07:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,33 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
   showMoreButton.addEventListener('click', () => {
     const parent = document.getElementById('additional-cards-container');
     parent.classList.toggle('hide');
-  })
-
-  // Select the "Show More" button
-  const showMoreButton = document.getElementById('show-more-button');
-  // Remove this line:
-  // const showMoreButton = document.querySelector('.show-more-button');
-
-
-  // Select the last three cards
-  const lastThreeCards = document.querySelectorAll('.card:nth-child(n+3)');
-
-  // Hide the last three cards initially
-  lastThreeCards.forEach(card => {
-    card.classList.add('hide');
-  });
-
-  // Add an event listener to the "Show More" button
-  showMoreButton.addEventListener('click', () => {
-    // Toggle the visibility of the last three cards
-    lastThreeCards.forEach(card => {
-      card.classList.toggle('hide');
-    });
 
     // Change the text of the "Show More" button to "Show All" if all the cards are visible
     const hiddenCards = document.querySelectorAll('.card.hide');
     if (hiddenCards.length === 0) {
       showMoreButton.textContent = 'Show All';
+    } else {
+      showMoreButton.textContent = 'Show More';
     }
+  })
+
+  // Select the last three cards
+  const lastThreeCards = document.querySelectorAll('.card:nth-child(n+4)');
+
+  // Hide the last three cards initially
+  lastThreeCards.forEach(card => {
+    card.classList.add('hide');
   });
 });
